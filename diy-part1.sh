c1c32009cda1f8b3afcf077f2841a8f55c086e98
guangyin53222 <532225662@qq.com>
2026-08-11 16:54:05 +0800

    Clean up package management in diy-part1.sh
    Removed unnecessary package removals and clones for OpenWrt feeds.

--- diy-part1.sh
+++ diy-part1.sh
@@ -59,13 +59,9 @@ echo " Adding luci-app-gecoosac"
 echo "============================================"
 rm -rf package/luci-app-gecoosac
 git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/luci-app-gecoosac
-# 移除 openwrt feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
-# 移除 openwrt feeds 过时的luci版本
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+
+
 
 # --- 6. 更新并安装插件源 ---
 echo "============================================"
